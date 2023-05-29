bf72a958dd1b5592eb18c5ebadf49aed3c424f78
Epicsociety <measkswho@gmail.com>
2023-05-29 14:35:13 +0300

    some stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,9 +4,30 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models.amenity import Amenity
 
 
 @app_views.route('/status', methods=['GET'])
 def index_status():
     """ returns status as JSON"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """ returns objects by type as json """
+    myDict = {
+            "amenities": storage.count(Amenity),
+            "cities": storage.count(City),
+            "places": storage.count(Place),
+            "reviews": storage.count(Review),
+            "states": storage.count(State),
+            "users": storage.count(User),
+            }
+    return jsonify(myDict)
