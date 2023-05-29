6acec6aa0294ad9f59cb8edc302d0fd76572ca8a
Epicsociety <measkswho@gmail.com>
2023-05-29 13:36:34 +0300

    staus of our api

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,8 +3,9 @@
 
 from flask import Blueprint
 
+
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-from api.v1.views.index import *
+from api.v1.views.index import *↴
 
-__all__ = ['index']  # Ignore PEP8 warning for wildcard import
+# __all__ = ['index']  # Ignore PEP8 warning for wildcard import
