bff573b3ff1b67f2d46be6249e38d2e938855cba
ndiritu-prisca <priscandiritu@outlook.com>
2023-05-30 11:33:04 +0000

    Updates to Users

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -13,10 +13,10 @@ def get_users():
     """A method to retrieve users """
     users = storage.all(User).values()
     user_dict = [user.to_dict() for user in users]
-    return user_dict
+    return jsonify(user_dict)
 
 
-@app_views.route('/users/<user_id>', methods=['GET'])
+@app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
 def get_user(user_id):
     """A method to retrieve a User object"""
     my_user = storage.get(User, user_id)
@@ -25,7 +25,7 @@ def get_user(user_id):
     return jsonify(my_user.to_dict())
 
 
-@app_views.route('/users/<user_id>', methods=['DELETE'])
+@app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
 def delete_user(user_id):
     """ deletes a user related to requested user_id """
     user = storage.get(User, user_id)
@@ -37,27 +37,27 @@ def delete_user(user_id):
         return jsonify({}), 200
 
 
-@app_views.route('/users', methods=['POST'])
+@app_views.route('/users', methods=['POST'], strict_slashes=False)
 def create_user():
     """ Creates a user """
     data = request.get_json()
-    if not data:
+    if data is None:
         abort(400, "Not a JSON")
-    # key = request.keys()
+
     email = data.get("email")
     if not email:
         abort(400, "Missing email")
     password = data.get("password")
     if not password:
         abort(400, "Missing password")
-    user = User(email=email, password=password)
-    user_dict = user.to_dict()
+    user = User(**data)
     storage.new(user)
     storage.save()
+    user_dict = user.to_dict()
     return jsonify(user_dict), 201
 
 
-@app_views.route('/users/<user_id>', methods=['PUT'])
+@app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
 def update_user(user_id):
     """ updates a user related to the user_id """
     user = storage.get(User, user_id)
@@ -67,8 +67,9 @@ def update_user(user_id):
     data = request.get_json()
     if not data:
         abort(400, "Not a JSON")
+    key_list = ['id', 'created_at', 'updated_at', 'email']
     for key, value in data.items():
-        if key == 'id' or key == 'created_at' or key == 'updated_at':
+        if key in key_list:
             pass
         else:
             setattr(user, key, value)
