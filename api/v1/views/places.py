47f49f55dca9363890fb47c59a9dff370f1a5bbe
Epicsociety <measkswho@gmail.com>
2023-05-30 15:36:17 +0300

    pycodestyl checked

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -29,7 +29,8 @@ def get_place(place_id):
     return jsonify(my_place.to_dict())
 
 
-@app_views.route('/places/<place_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/places/<place_id>', methods=['DELETE'],
+                 strict_slashes=False)
 def delete_place(place_id):
     """ deletes a place related to requested place_id """
     place = storage.get(Place, place_id)
