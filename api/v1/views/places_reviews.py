dc2d05a21a33adb0b15d136cd0adfea1e0e8f187
Epicsociety <measkswho@gmail.com>
2023-05-30 15:52:14 +0300

    added strict_slashes constraint

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -3,6 +3,7 @@
 
 from models.place import Place
 from models.review import Review
+from models.user import User
 from flask import jsonify, request, abort
 from api.v1.views import app_views
 from models import storage
@@ -12,14 +13,15 @@ from models import storage
                  methods=['GET'])
 def get_reviews(place_id):
     """A method to retrieve reviews """
-    place = storage.get(Place, place_id)
+    places = storage.get(Place, place_id)
     if not place:
         abort(404)
     reviews = [review.to_dict() for review in places.reviews]
-    return reviews
+    return jsonify(reviews)
 
 
-@app_views.route('/reviews/<review_id>', methods=['GET'])
+@app_views.route('/reviews/<review_id>', methods=['GET'],
+                 strict_slashes=False)
 def get_review(review_id):
     """A method to retrieve a Review object"""
     my_review = storage.get(Review, review_id)
@@ -28,7 +30,8 @@ def get_review(review_id):
     return jsonify(my_review.to_dict())
 
 
-@app_views.route('/reviews/<review_id>', methods=['DELETE'])
+@app_views.route('/reviews/<review_id>', methods=['DELETE'],
+                 strict_slashes=False)
 def delete_review(review_id):
     """ deletes a review related to requested review_id """
     review = storage.get(Review, review_id)
@@ -40,13 +43,14 @@ def delete_review(review_id):
         return jsonify({}), 200
 
 
-@app_views.route('/places/<place_id>/reviews', methods=['POST'])
+@app_views.route('/places/<place_id>/reviews', methods=['POST'],
+                 strict_slashes=False)
 def create_review(place_id):
     """ Creates a review """
     data = request.get_json()
     if not data:
         abort(400, "Not a JSON")
-    # key = request.keys()
+
     place = storage.get(Place, place_id)
     if place is None:
         abort(404)
@@ -63,14 +67,14 @@ def create_review(place_id):
     if not text:
         abort(400, "Missing text")
 
-    review = Review(text=text, place_id=place_id, user_id=user_id)
-    review_dict = review.to_dict()
+    review = Review(place_id=place_id, **data)
     storage.new(review)
     storage.save()
+    review_dict = review.to_dict()
     return jsonify(review_dict), 201
 
 
-@app_views.route('/reviews/<review_id>', methods=['PUT'])
+@app_views.route('/reviews/<review_id>', methods=['PUT'], strict_slashes=False)
 def update_review(city_id):
     """ updates a review related to the review_id """
     review = storage.get(Review, review_id)
